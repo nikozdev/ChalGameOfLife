bfa8800ec8cc3add146eab6e871750a22605c17b
nikozdev <nikzodev@nikzodev.net>
2024-08-15 12:31:06 +0300

    Rewrote everything into a single header-source pair;

--- src/exe.cpp
+++ src/exe.cpp
@@ -1,219 +1,266 @@
 #include "exe.hpp"
 
-namespace nExe {
-/* tOrganism */
-/** codetor **/
-tOrganism::tOrganism(std::optional<tOrganism> vAncestor)
-	: vAliveStepCount()
-	, vAliveStepLimit(
-			vAncestor ? vAncestor->vAliveStepLimit
-								: std::uniform_int_distribution<>(2, 8)(vRandomGen)
-		)
-	, vAliveValue(1)
-	, vReproStepCount()
-	, vReproStepLimit(
-			vAncestor ? vAncestor->vReproStepLimit
-								: std::uniform_int_distribution<>(1, 4)(vRandomGen)
-		)
-	, vReproIndex(vAncestor ? (vAncestor->vReproIndex + 1) : 0) {
-}
-/** actions **/
-std::optional<tOrganism> tOrganism::fRunStep() {
-	if(this->fVetAlive(0)) {
-		throw std::runtime_error("running step for a dead organism");
+namespace {//simulation
+
+	//{
+
+	///codetor
+
+	tSimBeing::tSimBeing(std::optional<tSimBeing> vAncestor)
+		: vAliveStepCount()
+		, vAliveStepLimit(
+				vAncestor ? vAncestor->vAliveStepLimit
+									: std::uniform_int_distribution<>(2, 8)(vRandomGen)
+			)
+		, vAliveValue(1)
+		, vReproStepCount()
+		, vReproStepLimit(
+				vAncestor ? vAncestor->vReproStepLimit
+									: std::uniform_int_distribution<>(1, 4)(vRandomGen)
+			)
+		, vReproIndex(vAncestor ? (vAncestor->vReproIndex + 1) : 0) {
 	}
-	std::optional<tOrganism> vChild;
-	if(this->vReproStepCount < this->vReproStepLimit) {
-		this->vReproStepCount += 1;
-	} else {
-		vChild.emplace(this->fTryRepro());
+
+	///actions
+
+	std::optional<tSimBeing> tSimBeing::fRunStep() {
+		if(this->fVetAlive(0)) {
+			throw std::runtime_error("running step for a dead organism");
+		}
+		std::optional<tSimBeing> vChild;
+		if(this->vReproStepCount < this->vReproStepLimit) {
+			this->vReproStepCount += 1;
+		} else {
+			vChild.emplace(this->fTryRepro());
+		}
+		if(this->vAliveStepCount < this->vAliveStepLimit) {
+			this->vAliveStepCount += 1;
+		} else {
+			this->fTryDeath();
+		}
+		return vChild;
 	}
-	if(this->vAliveStepCount < this->vAliveStepLimit) {
-		this->vAliveStepCount += 1;
-	} else {
-		this->fTryDeath();
-	}
-	return vChild;
-}
-bool tOrganism::fTryAntibio() {
-	if(this->fVetAlive(0)) {
-		throw std::runtime_error("trying antibio on a dead organism");
-	}
-	auto vAntibioSurviveChance = std::
-		uniform_int_distribution<>(1, vAntibioKillingChance)(vRandomGen);
-	if(vAntibioSurviveChance > this->vAntibioSurviveChance) {
-		return this->fTryDeath();
-	} else {
-		return 0;
-	}
-}
-bool tOrganism::fTryDeath() {
-	if(this->fVetAlive(0)) {
-		throw std::runtime_error("trying death on a dead organism");
-	}
-	this->vAliveValue = 0;
-	return 1;
-}
-std::optional<tOrganism> tOrganism::fTryRepro() {
-	if(this->fVetAlive(0)) {
-		throw std::runtime_error("trying death on a dead organism");
-	}
-	return tOrganism(*this);
-}
-}//namespace nExe
-namespace nExe {
-bool fRun(int vArgC, char **vArgV) {
-	auto vStepIndex = 0;
-	fmt::println(stdout, "Simulation is being started...");
-	auto vCountUse = 0;
-	auto vCountMin = 1;
-	auto vCountMax = 20;
-	while(vCountMin > vCountUse || vCountMax < vCountUse) {
-		fmt::println(stdout, "We cannot create {} organisms.", vCountUse);
-		fmt::println(
-			stdout,
-			"Type the amount of organisms from {} to {} (inclusively):",
-			vCountMin,
-			vCountMax
-		);
-		try {
-			std::string vInput;
-			std::size_t vReadP;
-			std::getline(std::cin, vInput);
-			auto vCountGot = std::stoi(vInput, &vReadP);
-			if(vReadP < vInput.length()) {
-				throw std::invalid_argument("Extra characters in the input!");
-			}
-			vCountUse = vCountGot;
-		} catch(const std::exception &vError) {
-			fmt::println(stdout, "Read failure! Still having {};", vCountUse);
+
+	bool tSimBeing::fTryAntibio() {
+		if(this->fVetAlive(0)) {
+			throw std::runtime_error("trying antibio on a dead organism");
+		}
+		auto vAntibioSurviveChance = std::
+			uniform_int_distribution<>(1, vAntibioKillingChance)(vRandomGen);
+		if(vAntibioSurviveChance > this->vAntibioSurviveChance) {
+			return this->fTryDeath();
+		} else {
+			return 0;
 		}
 	}
-	auto vArray			 = std::vector<tOrganism>(vCountUse);
-	auto vChildArray = std::vector<std::optional<tOrganism>>();
-	auto vDeathCount = 0;
-	bool vRunFlag		 = 1;
-	while(vRunFlag) {
-		fmt::println(stdout, "Step index: {};", vStepIndex);
-		fmt::println(stdout, "Alive count: {};", vArray.size());
-		fmt::println(stdout, "Death count: {};", vDeathCount);
-		fmt::println(stdout, "");
-		char vInput = '\0';
-		fmt::println(
-			stdout,
-			"Choose your action:"
-			"\n- 's' to step forward in time;"
-			"\n- 'a' to use AntiBiotics;"
-			"\n- 'd' to display information about each organism;"
-			"- 'q' to quit;"
-		);
-		try {
-			std::cin >> vInput;
-		} catch(const std::exception &vError) {
-			fmt::println(stdout, "Read failure! Not gonna do anything;");
+
+	bool tSimBeing::fTryDeath() {
+		if(this->fVetAlive(0)) {
+			throw std::runtime_error("trying death on a dead SimBeing");
 		}
-		fmt::println(stdout, "");
-		//would like to use an unordered_map of callbacks here
-		//but this would be an overkill for a few actions
-		//especially for these callbacks to control the context
-		switch(vInput) {
-		case 's': {
-			fmt::println("Step forward is being done.");
-			for(auto &vEntry: vArray) {
-				vChildArray.push_back(vEntry.fRunStep());
-			}
-			fmt::println("Step forward has been done.");
-			vStepIndex++;
-		} break;
+		this->vAliveValue = 0;
+		return 1;
+	}
+
+	std::optional<tSimBeing> tSimBeing::fTryRepro() {
+		if(this->fVetAlive(0)) {
+			throw std::runtime_error("trying death on a dead organism");
+		}
+		return tSimBeing(*this);
+	}
+
+	//}
+
+}
+
+namespace {//system
+
+	//{
+
+	///codetor
+
+	tApp::tApp(int vArgC, char **vArgV)
+		: QApplication(vArgC, vArgV)
+		, vWin{std::make_shared<tWin>()}
+		, vSimStepIndex(0) {
+		this->setStyleSheet(
+			"QWidget {"
+			" background: black;"
+			" alternate-background-color: dark-gray;"
+			" accent-color: light-gray;"
+			" color: cyan;"
+			" placeholder-text-color: dark-cyan;"
+			" selection-color: light-cyan;"
+			"}"
+		);
+	}//ctor
+
+	///actions
+
+	bool tApp::fTryEventKeyPress(QKeyEvent *vQKeyEvent) {
+		auto vQKeyBytes = vQKeyEvent->text().toUtf8().constData();
+		fLogErr("vQKeyBytes", "{};", vQKeyBytes);
+		auto vQKeyValue = vQKeyBytes[0];
+		switch(vQKeyValue) {
 		case 'a': {
-			fmt::println("Using AntiBiotics:");
-			for(auto &vEntry: vArray) {
+			auto vCount = this->vSimBeingArray.size();
+			for(auto vIndex = 0; vIndex < vCount; vIndex++) {
+				auto &vEntry = this->vSimBeingArray[vIndex];
 				vEntry.fTryAntibio();
 			}
 		} break;
-		case 'd': {
-			fmt::println("Organism status display:");
-			for(auto vIndex = 0; vIndex < vArray.size(); vIndex++) {
-				const auto &vEntry = vArray[vIndex];
-				const auto	vOrder = vIndex + 1;
-				fmt::println(
-					"- [{}]"
-					" Alive: {};"
-					" AliveStepCount: {};"
-					" AliveStepLimit: {};"
-					" ReproStepCount: {};"
-					" ReproStepLimit: {};"
-					" ReproIndex: {};"
-					"",
-					vOrder,
-					vEntry.fVetAlive(1),
-					vEntry.fGetAliveStepCount(),
-					vEntry.fGetAliveStepLimit(),
-					vEntry.fGetReproStepCount(),
-					vEntry.fGetReproStepLimit(),
-					vEntry.fGetReproIndex()
-				);
-			}
+		case 's': {
+			this->vSimStepIndex += 1;
+			this->vWin->fGetKeyOutput()
+				->setText("Simulation step forward has been done;");
 		} break;
 		case 'q': {
-			fmt::println(stdout, "Simulation is being finished...");
-			vRunFlag = 0;
+			this->quit();
 		} break;
 		default: {
-			fmt::println(stdout, "Unknown action ({})", vInput);
 		} break;
 		}
-		fmt::println(stdout, "");
-		//insert children into the main array
-		for(auto &vChildEntry: vChildArray) {
-			if(vChildEntry) {
-				vArray.push_back(*vChildEntry);
-			}
-		}
-		vChildArray.clear();
-		//keeping alive, leaving the dead
-		auto vAliveCount = 0;
-		for(auto &vEntry: vArray) {
-			vAliveCount += vEntry.fVetAlive(1);
-		}
-		if(vAliveCount != vArray.size()) {
-			decltype(vArray) vAliveArray;
-			vAliveArray.reserve(vAliveCount);
-			for(auto &vEntry: vArray) {
-				if(vEntry.fVetAlive(1)) {
-					vAliveArray.push_back(vEntry);
-				} else {
-					vDeathCount++;
-				}
-			}
-			vArray = std::move(vAliveArray);
-		}
+		return 1;
 	}
-	fmt::println(stdout, "Simulation has been finished...");
-	return 1;
+
+	//}
+
+	//{
+
+	///codetor
+
+	tWin::tWin(QWidget *vParentPtr, Qt::WindowFlags vFlags)
+		: QWidget(vParentPtr, vFlags)
+		, vLayout{std::make_shared<QVBoxLayout>(this)}
+		, vKeyOutput{std::make_shared<QTextEdit>("key output", this)}
+		, vKeyPrompt{std::make_shared<QTextEdit>("key prompt", this)}
+		, vSimStateReport{std::make_shared<QTextEdit>("sim state report", this)}
+		, vSimBeingReport{std::make_shared<tSimBeingReport>(this)
+			} {
+		//vLayout
+
+		this->setLayout(this->vLayout.get());
+
+		//vKeyOutput
+
+		this->vLayout->addWidget(this->vKeyOutput.get(), 2);
+		this->vKeyOutput->setReadOnly(1);
+
+    {
+      auto vHSep = new QFrame(this);
+      this->vLayout->addWidget(vHSep, 1);
+      vHSep->setFrameShape(QFrame::HLine);
+      vHSep->setLineWidth(1);
+    }
+
+		//vKeyPrompt
+
+		this->vLayout->addWidget(this->vKeyPrompt.get(), 3);
+		this->vKeyPrompt->setReadOnly(1);
+		this->vKeyPrompt->setText(
+			"Hit one of the keys:"
+			"\n- 's' to add 1 step to the simulation progress;"
+			"\n- 'a' to use antibiotics;"
+			"\n- 'q' to quit;"
+		);
+
+    {
+      auto vHSep = new QFrame(this);
+      this->vLayout->addWidget(vHSep, 1);
+      vHSep->setFrameShape(QFrame::HLine);
+      vHSep->setLineWidth(1);
+    }
+
+		//vSimStateReport
+
+		this->vLayout->addWidget(this->vSimStateReport.get(), 2);
+		this->vSimStateReport->setReadOnly(1);
+
+    {
+      auto vHSep = new QFrame(this);
+      this->vLayout->addWidget(vHSep, 1);
+      vHSep->setFrameShape(QFrame::HLine);
+      vHSep->setLineWidth(1);
+    }
+
+		//vSimBeingReport
+
+		this->vLayout->addWidget(this->vSimBeingReport.get(), 8);
+		for(auto vUnitIndex = 0; vUnitIndex < dUnitCount; vUnitIndex++) {
+			auto vUnitOrder = vUnitIndex + 1;
+			auto vUnitLabel = fmt::format("[{}]", vUnitOrder);
+			this->vSimBeingReport->fAddWidget(new QLabel(vUnitLabel.c_str(), this));
+		}
+
+		//Font
+
+		auto vFontFpath = "data/FiraMono-Bold.ttf";
+		fLogErr("vFontFpath", "\"{}\";", vFontFpath);
+		auto vFontIndex = QFontDatabase::addApplicationFont(vFontFpath);
+		fLogErr("vFontIndex", "{};", vFontIndex);
+		auto vFontIdentArray = QFontDatabase::applicationFontFamilies(vFontIndex);
+		fLogErr("vFontIdentArray.length()", "{};", vFontIdentArray.length());
+		auto vFontIdent = vFontIdentArray.at(0);
+		fLogErr("vFontIdent", "\"{}\";", vFontIdent.toUtf8().constData());
+		auto vFontEntry = QFont(vFontIdent, 16);
+		vFontEntry.setBold(true);
+		this->setFont(vFontEntry);
+
+		//Window show
+
+		this->setWindowTitle(dProjName);
+
+		auto vRect = QGuiApplication::primaryScreen()->geometry();
+		this->resize(vRect.width() / 2, vRect.height());
+
+		this->show();
+	}//ctor
+
+	///actions
+
+	void tWin::keyPressEvent(QKeyEvent *vQKeyEvent) {
+		auto vApp = reinterpret_cast<tApp *>(QApplication::instance());
+		vApp->fTryEventKeyPress(vQKeyEvent);
+		QWidget::keyPressEvent(vQKeyEvent);
+	}
+
+	//}
+
+	//{
+
+	tSimBeingReport::tSimBeingReport(QWidget *vParentPtr)
+		: QScrollArea(vParentPtr), vLayout{std::make_shared<QVBoxLayout>(this)} {
+		this->setLayout(this->vLayout.get());
+	}
+
+	void tSimBeingReport::fAddWidget(QWidget *vWidgetPtr) {
+		this->vLayout->addWidget(vWidgetPtr, 1);
+	}
+
+	//}
+
 }
-bool fTry(int vArgC, char **vArgV) {
-	fmt::println(stderr, "[exe]=(");
-	fLogErr("vArgC", "{};", vArgC);
-	fLogErr("vArgV[0]", "\"{}\";", vArgV[0]);
-	fLogErr("dProjName", "\"{}\";", dProjName);
-	fLogErr("dWorkPath", "\"{}\";", std::filesystem::current_path().c_str());
-	nGui::fTry(vArgC, vArgV);
-	fmt::println(stderr, ")=[exe]");
-	return 1;
-}
-}//namespace nExe
+
+//application entry point
 
 int main(int vArgC, char **vArgV) {
+	fmt::println(stderr, "[main]=(");
 	try {
-		if constexpr(1) {
-			return nExe::fRun(vArgC, vArgV) ? 0 : 1;
-		} else {
-			return nExe::fTry(vArgC, vArgV) ? 0 : 1;
+		fLogErr("vArgC", "{};", vArgC);
+		fLogErr("vArgV[0]", "\"{}\";", vArgV[0]);
+		fLogErr("dProjName", "\"{}\";", dProjName);
+		fLogErr("dWorkPath", "\"{}\";", std::filesystem::current_path().c_str());
+		fLogErr("QT_VERSION_STR", "\"{}\";", QT_VERSION_STR);
+		auto vApp = tApp(vArgC, vArgV);
+		auto vErr = vApp.exec();
+		fLogErr("error", "{};", vErr);
+		if(vErr) {
+			return vErr;
 		}
 	} catch(const std::exception &vError) {
-		fLogErr("[vError]", "\"{}\";", vError.what());
+		fLogErr("[exception]", "\"{}\";", vError.what());
+		return 1;
 	}
-
-	return 1;
+	fmt::println(stderr, ")=[main]");
+	return 0;
 }
--- src/exe.hpp
+++ src/exe.hpp
@@ -3,82 +3,158 @@
 #ifndef dChalGameOfLife_exe_hpp
 #define dChalGameOfLife_exe_hpp
 
-#include "gui.hpp"
+namespace {//simulation
 
-namespace nExe {
-class tOrganism {
-public://consdef
+	/* simulation being class
+	 * - reproduces itself once in a constant amount of time
+	 * - dies after a constant amount of time
+	 * - can have a parent, inheriting it's timing settings
+	 */
+	class tSimBeing {
+	public://consdef
 
-	static constexpr auto vAntibioSurviveChance = 3;
-	static constexpr auto vAntibioKillingChance = 10;
-	static_assert(
-		vAntibioSurviveChance < vAntibioKillingChance,
-		"survival chance is included in the killing chance;"
-		"an organism survives if a random value [1,AntibioKillingChance]"
-		" is less than or equal to the AntibioSurviveChance;"
-		"an organism dies if a random value [1,AntibioKillingChance]"
-		" is more than the AntibioSurviveChance;"
-	);
+		static constexpr auto vAntibioSurviveChance = 3;
+		static constexpr auto vAntibioKillingChance = 10;
+		static_assert(
+			vAntibioSurviveChance < vAntibioKillingChance,
+			"survival chance is included in the killing chance;"
+			"an organism survives if a random value [1,AntibioKillingChance]"
+			" is less than or equal to the AntibioSurviveChance;"
+			"an organism dies if a random value [1,AntibioKillingChance]"
+			" is more than the AntibioSurviveChance;"
+		);
 
-public://codetor
+	public://codetor
 
-	tOrganism(std::optional<tOrganism> vAncestor = {});
+		tSimBeing(std::optional<tSimBeing> vAncestor = {});
 
-public://getters
+	public://getters
 
-	inline auto fGetAliveStepCount() const {
-		return this->vAliveStepCount;
-	}
-	inline auto fGetAliveStepLimit() const {
-		return this->vAliveStepLimit;
-	}
+		inline auto fGetAliveStepCount() const {
+			return this->vAliveStepCount;
+		}
+		inline auto fGetAliveStepLimit() const {
+			return this->vAliveStepLimit;
+		}
 
-	inline auto fGetReproStepCount() const {
-		return this->vReproStepCount;
-	}
-	inline auto fGetReproStepLimit() const {
-		return this->vReproStepLimit;
-	}
-	inline auto fGetReproIndex() const {
-		return this->vReproIndex;
-	}
+		inline auto fGetReproStepCount() const {
+			return this->vReproStepCount;
+		}
+		inline auto fGetReproStepLimit() const {
+			return this->vReproStepLimit;
+		}
+		inline auto fGetReproIndex() const {
+			return this->vReproIndex;
+		}
 
-public://vetters
+	public://vetters
 
-	inline auto fVetAlive(bool vAliveValue = 1) const {
-		return (this->vAliveValue == vAliveValue);
-	}
+		inline auto fVetAlive(bool vAliveValue = 1) const {
+			return (this->vAliveValue == vAliveValue);
+		}
 
-public://actions
+	public://actions
 
-	std::optional<tOrganism> fRunStep();
+		std::optional<tSimBeing> fRunStep();
 
-	bool fTryAntibio();
+		bool fTryAntibio();
 
-private://actions
+	private://actions
 
-	bool fTryDeath();
+		bool fTryDeath();
 
-	std::optional<tOrganism> fTryRepro();
+		std::optional<tSimBeing> fTryRepro();
 
-private://vardefs
+	private://vardefs
 
-	//lifetime
-	long			 vAliveStepCount;//how many steps since birth?
-	const long vAliveStepLimit;//how many steps until death?
-	bool			 vAliveValue;		 //are we alive?
+		//lifetime
+		long			 vAliveStepCount;//how many steps since birth?
+		const long vAliveStepLimit;//how many steps until death?
+		bool			 vAliveValue;		 //are we alive?
 
-	//reproduction
-	long			 vReproStepCount;//how many steps since the last reproduction?
-	const long vReproStepLimit;//how many steps until the next reproduction?
-	const long vReproIndex;		 //the current generation number
-};
-}//namespace nExe
-namespace nExe {
-bool fRun(int vArgC, char **vArgV);
-bool fTry(int vArgC, char **vArgV);
-}//namespace nExe
+		//reproduction
+		long			 vReproStepCount;//how many steps since the last reproduction?
+		const long vReproStepLimit;//how many steps until the next reproduction?
+		const long vReproIndex;		 //the current generation number
 
+	};//tSimBeing
+
+}//simulation
+
+namespace {//system
+
+	/* application giga-class
+	 * - creates and holds tWin instance
+	 */
+	class tWin;
+	class tApp: public QApplication {
+	public:
+
+		tApp(int vArgC, char **vArgV);
+
+		bool fTryEventKeyPress(QKeyEvent *vQKeyEvent);
+
+	private:
+
+		std::shared_ptr<tWin> vWin;
+
+		int vSimStepIndex;
+
+		QList<tSimBeing> vSimBeingArray;
+
+	};//tApp
+
+	/* window of application
+	 * - creates and holds tSimBeingReport
+	 */
+	class tSimBeingReport;
+	class tWin: public QWidget {
+	public://codetor
+
+		tWin(
+			QWidget *vParentPtr = nullptr, Qt::WindowFlags vFlags = Qt::WindowFlags()
+		);
+
+	public://actions
+
+		void keyPressEvent(QKeyEvent *vQKeyEvent) override;
+
+	public://getters
+
+		auto fGetKeyOutput() {
+			return this->vKeyOutput;
+		}
+
+	private://vardefs
+
+		std::shared_ptr<QVBoxLayout> vLayout;
+
+		std::shared_ptr<QTextEdit> vKeyPrompt;
+		std::shared_ptr<QTextEdit> vKeyOutput;
+
+		std::shared_ptr<QTextEdit>			 vSimStateReport;
+		std::shared_ptr<tSimBeingReport> vSimBeingReport;
+
+	};//tWin
+
+	/* window to monitor states of all organisms
+	 */
+	class tSimBeingReport: public QScrollArea {
+	public:
+
+		tSimBeingReport(QWidget *vParentPtr);
+
+		void fAddWidget(QWidget *vWidgetPtr);
+
+	private:
+
+		std::shared_ptr<QVBoxLayout> vLayout;
+
+	};//tSimBeingReport
+
+}//system
+
+//entry point
 extern int main(int vArgC, char **vArgV);
 
 #endif//dChalGameOfLife_exe_hpp
