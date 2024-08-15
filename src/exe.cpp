ecc71846f028a2bea9a2cd34f8de99bfaa657ea4
nikozdev <nikzodev@nikzodev.net>
2024-08-15 17:02:53 +0300

    Implemented qt6 meta-object usage to use signals and slots;

--- src/exe.cpp
+++ src/exe.cpp
@@ -1,370 +1,385 @@
 #include "exe.hpp"
 
-namespace {//util
+//{
 
-	void fAddGuiHorLine(auto vParent, auto &vLayout) {
-		auto vHSep = new QFrame(vParent);
-		vLayout->addWidget(vHSep, 1);
-		vHSep->setFrameShape(QFrame::HLine);
-		vHSep->setLineWidth(1);
-	}
-
+void fAddGuiHorLine(auto vParent, auto &vLayout) {
+	auto vHSep = new QFrame(vParent);
+	vLayout->addWidget(vHSep, 1);
+	vHSep->setFrameShape(QFrame::HLine);
+	vHSep->setLineWidth(1);
 }
 
-namespace {//simulation
+//}
 
-	//{
+//{
 
-	///codetor
+///{
 
-	tSimBeing::tSimBeing(std::optional<tSimBeing> vAncestor)
-		: vAliveStepCount()
-		, vAliveStepLimit(
-				vAncestor ? vAncestor->vAliveStepLimit
-									: std::uniform_int_distribution<>(2, 8)(vRandomGen)
-			)
-		, vAliveValue(1)
-		, vReproStepCount()
-		, vReproStepLimit(
-				vAncestor ? vAncestor->vReproStepLimit
-									: std::uniform_int_distribution<>(1, 4)(vRandomGen)
-			)
-		, vReproIndex(vAncestor ? (vAncestor->vReproIndex + 1) : 0) {
-	}
+//codetor
 
-	///actions
-
-	std::optional<tSimBeing> tSimBeing::fRunStep() {
-		if(this->fVetAlive(0)) {
-			fLogErr("fRunStep", "Running step for a dead organism!");
-		}
-		std::optional<tSimBeing> vChild;
-		if(this->vReproStepCount < this->vReproStepLimit) {
-			this->vReproStepCount += 1;
-		} else {
-			vChild.emplace(this->fTryRepro());
-		}
-		if(this->vAliveStepCount < this->vAliveStepLimit) {
-			this->vAliveStepCount += 1;
-		} else {
-			this->fTryDeath();
-		}
-		return vChild;
-	}
-
-	bool tSimBeing::fTryAntibio() {
-		if(this->fVetAlive(0)) {
-			fLogErr("fTryAntibio", "Trying antibio on a dead organism!");
-		}
-		auto vAntibioSurviveChance = std::
-			uniform_int_distribution<>(1, vAntibioKillingChance)(vRandomGen);
-		if(vAntibioSurviveChance > this->vAntibioSurviveChance) {
-			return this->fTryDeath();
-		} else {
-			return 0;
-		}
-	}
-
-	bool tSimBeing::fTryDeath() {
-		if(this->fVetAlive(0)) {
-			fLogErr("fTryDeath", "Trying death on a dead organism!");
-		}
-		this->vAliveValue = 0;
-		return 1;
-	}
-
-	std::optional<tSimBeing> tSimBeing::fTryRepro() {
-		if(this->fVetAlive(0)) {
-			fLogErr("fTryRepro", "Trying repro on a dead organism!");
-		}
-		return tSimBeing(*this);
-	}
-
-	//}
-
+tSimBeing::tSimBeing(std::optional<tSimBeing> vAncestor)
+	: vAliveStepCount()
+	, vAliveStepLimit(
+			vAncestor ? vAncestor->vAliveStepLimit
+								: std::uniform_int_distribution<>(2, 8)(vRandomGen)
+		)
+	, vAliveValue(1)
+	, vReproStepCount()
+	, vReproStepLimit(
+			vAncestor ? vAncestor->vReproStepLimit
+								: std::uniform_int_distribution<>(1, 4)(vRandomGen)
+		)
+	, vReproIndex(vAncestor ? (vAncestor->vReproIndex + 1) : 0) {
 }
 
-namespace {//system
+//actions
 
-	//{
-
-	///codetor
-
-	tApp::tApp(int vArgC, char **vArgV)
-		: QApplication(vArgC, vArgV), vWindow{new tAppWindow()}, vSimStepIndex(0) {
-		this->setStyleSheet(
-			"QWidget {"
-			" background: black;"
-			" alternate-background-color: dark-gray;"
-			" accent-color: light-gray;"
-			" color: cyan;"
-			" placeholder-text-color: dark-cyan;"
-			" selection-color: light-cyan;"
-			"}"
-		);
+std::optional<tSimBeing> tSimBeing::fRunStep() {
+	if(this->fVetAlive(0)) {
+		fLogErr("fRunStep", "Running step for a dead organism!");
 	}
+	std::optional<tSimBeing> vChild;
+	if(this->vReproStepCount < this->vReproStepLimit) {
+		this->vReproStepCount += 1;
+	} else {
+		//vChild.emplace(this->fTryRepro());
+	}
+	if(this->vAliveStepCount < this->vAliveStepLimit) {
+		this->vAliveStepCount += 1;
+	} else {
+		this->fTryDeath();
+	}
+	//return vChild;
+  return {};
+}
 
-	///actions
+bool tSimBeing::fTryAntibio() {
+	if(this->fVetAlive(0)) {
+		fLogErr("fTryAntibio", "Trying antibio on a dead organism!");
+	}
+	auto vAntibioSurviveChance = std::
+		uniform_int_distribution<>(1, vAntibioKillingChance)(vRandomGen);
+	if(vAntibioSurviveChance > this->vAntibioSurviveChance) {
+		return this->fTryDeath();
+	} else {
+		return 0;
+	}
+}
 
-	void tApp::fRunSim(int vSimBeing1stCount) {
-		if(this->vSimBeing1stCount) {
-			fLogErr("fRunSim Error", "Cannot set simulation twice!");
+bool tSimBeing::fTryDeath() {
+	if(this->fVetAlive(0)) {
+		fLogErr("fTryDeath", "Trying death on a dead organism!");
+	}
+	this->vAliveValue = 0;
+	return 1;
+}
+
+std::optional<tSimBeing> tSimBeing::fTryRepro() {
+	if(this->fVetAlive(0)) {
+		fLogErr("fTryRepro", "Trying repro on a dead organism!");
+	}
+	return tSimBeing(*this);
+}
+
+///}
+
+//}
+
+//{
+
+///{
+
+//codetor
+
+tApp::tApp(int vArgC, char **vArgV)
+	: QApplication(vArgC, vArgV), vWindow{new tAppWindow()}, vSimStepIndex(0) {
+	this->setStyleSheet(
+		"QWidget {"
+		" background: black;"
+		" alternate-background-color: dark-gray;"
+		" accent-color: light-gray;"
+		" color: cyan;"
+		" placeholder-text-color: dark-cyan;"
+		" selection-color: light-cyan;"
+		"}"
+	);
+}
+
+//actions
+
+void tApp::fRunSim(int vSimBeing1stCount) {
+	if(this->vSimBeing1stCount) {
+		fLogErr("fRunSim Error", "Cannot set simulation twice!");
+	}
+	this->vSimBeing1stCount = vSimBeing1stCount;
+	this->vSimBeingArray		= QVector<tSimBeing>(vSimBeing1stCount);
+	emit this->sRunSimCall();
+}
+
+void tApp::fRunEventKeyPress(QKeyEvent *vQKeyEvent) {
+	auto vQKeyBytes = vQKeyEvent->text().toLower().toUtf8().constData();
+	fLogErr("vQKeyBytes", "{};", vQKeyBytes);
+	auto vQKeyValue = vQKeyBytes[0];
+	switch(vQKeyValue) {
+	case 'a': {
+		auto vTotalCount = this->vSimBeingArray.size();
+		auto vAliveCount = 0;
+		for(auto vIndex = 0; vIndex < vTotalCount; vIndex++) {
+			auto &vEntry = this->vSimBeingArray[vIndex];
+			vEntry.fTryAntibio();
+			vAliveCount += vEntry.fVetAlive(1);
 		}
-		this->vSimBeing1stCount = vSimBeing1stCount;
-		this->vSimBeingArray		= QVector<tSimBeing>(vSimBeing1stCount);
-		this->vWindow->fSetSimWindow();
-	}
-
-	void tApp::fAddSimStep() {
-		this->vSimStepIndex++;
-	}
-
-	void tApp::fRunEventKeyPress(QKeyEvent *vQKeyEvent) {
-		auto vQKeyBytes = vQKeyEvent->text().toLower().toUtf8().constData();
-		fLogErr("vQKeyBytes", "{};", vQKeyBytes);
-		auto vQKeyValue = vQKeyBytes[0];
-		switch(vQKeyValue) {
-		case 'a': {
-			auto vTotalCount = this->vSimBeingArray.size();
-			auto vAliveCount = 0;
-			for(auto vIndex = 0; vIndex < vTotalCount; vIndex++) {
-				auto &vEntry = this->vSimBeingArray[vIndex];
-				vEntry.fTryAntibio();
-				vAliveCount += vEntry.fVetAlive(1);
-			}
-			this->vWindow->vSimWindow->vOutput->setText(
-				fmt::format(
-					"[{}]: "
-					"Using antibiotics...\n"
-					"({}/{} left alive);",
-					vQKeyValue,
-					vAliveCount,
-					vTotalCount
-				)
-					.c_str()
-			);
-		} break;
-		case 's': {
-			auto vSimStepWas = this->vSimStepIndex;
-			this->fAddSimStep();
-			auto vSimStepNow = this->vSimStepIndex;
-			this->vWindow->vSimWindow->vOutput->setText(
-				fmt::format(
-					"[{}]: "
-					"Taking 1 more step of simulation\n"
-					"(from {} to {});",
-					vQKeyValue,
-					vSimStepWas,
-					vSimStepNow
-				)
-					.c_str()
-			);
-		} break;
-		case 'q': {
-			this->vWindow->vSimWindow->vOutput->setText(
-				fmt::format(
-					"[{}]: "
-					"Bye-bye!",
-					vQKeyValue
-				)
-					.c_str()
-			);
-			fLogErr("TryKeyPress", "Quit is being done;");
-			this->quit();
-			fLogErr("TryKeyPress", "Quit has been done;");
-		} break;
-		default: {
-			this->vWindow->vSimWindow->vOutput
-				->setText(fmt::format("[{}]: Nothing to do;", vQKeyValue).c_str());
-		} break;
-		}
-	}
-
-	//}
-
-	//{
-
-	///codetor
-
-	tAppWindow::tAppWindow(QWidget *vParentPtr, Qt::WindowFlags vFlags)
-		: QMainWindow(vParentPtr, vFlags)
-		, v1stWindow{new t1stWindow(this)}
-		, vSimWindow{new tSimWindow(this)}
-		, vStack{new QStackedWidget(this)} {
-		fLogErr("AppWindow", "ctor");
-		if constexpr(1) {//Stack
-
-			this->setCentralWidget(this->vStack.get());
-
-			this->vStack->addWidget(this->v1stWindow.get());
-			this->vStack->addWidget(this->vSimWindow.get());
-
-			this->vStack->setCurrentIndex(0);
-		}
-		if constexpr(1) {//Fonts
-
-			auto vFontFpath = "data/FiraMono-Bold.ttf";
-			fLogErr("vFontFpath", "\"{}\";", vFontFpath);
-
-			auto vFontIndex = QFontDatabase::addApplicationFont(vFontFpath);
-			fLogErr("vFontIndex", "{};", vFontIndex);
-
-			auto vFontIdentArray = QFontDatabase::applicationFontFamilies(vFontIndex);
-			fLogErr("vFontIdentArray.length()", "{};", vFontIdentArray.length());
-
-			auto vFontIdent = vFontIdentArray.at(0);
-			fLogErr("vFontIdent", "\"{}\";", vFontIdent.toUtf8().constData());
-
-			auto vFontEntry = QFont(vFontIdent, 16);
-			vFontEntry.setBold(true);
-
-			this->setFont(vFontEntry);
-		}
-		if constexpr(1) {//Window show
-
-			this->setWindowTitle(dProjName);
-
-			auto vRect = QGuiApplication::primaryScreen()->geometry();
-			this->resize(vRect.width() / 2, vRect.height());
-
-			this->show();
-		}
-	}
-
-	//slots
-
-	void tAppWindow::fSetSimWindow() {
-		this->vStack->setCurrentIndex(1);
-	}
-
-	//}
-
-	//{
-
-	//codetor
-
-	t1stWindow::t1stWindow(tAppWindow *vAppWindow)
-		: QWidget(vAppWindow)
-		, vLayout{new QVBoxLayout(this)}
-		, vPrompt{new QTextEdit("start prompt", this)}
-		, vButton{new QPushButton("start", this)} {
-		fLogErr("1stWindow", "ctor");
-		this->setLayout(this->vLayout.get());
-
-		this->vLayout->addWidget(this->vPrompt.get(), 4);
-		this->vPrompt->setReadOnly(1);
-		this->vPrompt->setText(
-			"Welcome to the micro-organism simulation!\n"
-			"Before we run the simulation,"
-			" we want you to configure this."
-			"\n"
-			"The configuration is gonna start"
-			" as soon as you hit the button below."
-		);
-
-		this->vLayout->addWidget(this->vButton.get(), 1);
-		this->vButton->setText("Config");
-		connect(
-			this->vButton.get(), &QPushButton::clicked, this, &t1stWindow::fRunSimConfig
-		);
-	}
-
-	//slots
-
-	void t1stWindow::fRunSimConfig() {
-		bool vInputFlag;
-		auto vSimBeing1stCount = QInputDialog::getInt(
-			this,
-			"Starting Simulation",
+		this->vWindow->vSimWindow->vOutput->setText(
 			fmt::format(
-				"Enter the number of micro-organisms from {} to {}.",
-				this->vSimBeingMinCount,
-				this->vSimBeingMaxCount
+				"[{}]: "
+				"Using antibiotics...\n"
+				"({}/{} left alive);",
+				vQKeyValue,
+				vAliveCount,
+				vTotalCount
 			)
-				.c_str(),
-			0,
-			1,
-			20,
-			1,
-			&vInputFlag
+				.c_str()
 		);
-		if(vInputFlag) {
-			auto vApp = static_cast<tApp *>(QApplication::instance());
-			vApp->fRunSim(vSimBeing1stCount);
-		}
-	}
-
-	//}
-
-	//{
-
-	tSimWindow::tSimWindow(tAppWindow *vAppWindow)
-		: QWidget(vAppWindow)
-		, vLayout{new QVBoxLayout(this)}
-		, vPrompt{new QTextEdit("key prompt", this)}
-		, vOutput{new QTextEdit("key output", this)}
-		, vReport{new tSimReport(this)} {
-		fLogErr("SimWindow", "ctor");
-		this->setLayout(this->vLayout.get());
-
-		this->vLayout->addWidget(this->vPrompt.get(), 3);
-		this->vPrompt->setReadOnly(1);
-		this->vPrompt->setText(
-			"Hit one of the keys:"
-			"\n- 's' to add 1 step to the simulation progress;"
-			"\n- 'a' to use antibiotics;"
-			"\n- 'q' to quit;"
+	} break;
+	case 's': {
+		auto vSimStepWas = this->vSimStepIndex;
+		this->fAddSimStep();
+		auto vSimStepNow = this->vSimStepIndex;
+		this->vWindow->vSimWindow->vOutput->setText(
+			fmt::format(
+				"[{}]: "
+				"Taking 1 more step of simulation\n"
+				"(from {} to {});",
+				vQKeyValue,
+				vSimStepWas,
+				vSimStepNow
+			)
+				.c_str()
 		);
-
-		fAddGuiHorLine(this, this->vLayout);
-
-		this->vLayout->addWidget(this->vOutput.get(), 2);
-		this->vOutput->setReadOnly(1);
-
-		fAddGuiHorLine(this, this->vLayout);
-
-		this->vLayout->addWidget(this->vReport.get(), 8);
+	} break;
+	case 'q': {
+		this->vWindow->vSimWindow->vOutput->setText(
+			fmt::format(
+				"[{}]: "
+				"Bye-bye!",
+				vQKeyValue
+			)
+				.c_str()
+		);
+		fLogErr("TryKeyPress", "Quit is being done;");
+		this->quit();
+		fLogErr("TryKeyPress", "Quit has been done;");
+	} break;
+	default: {
+		this->vWindow->vSimWindow->vOutput
+			->setText(fmt::format("[{}]: Nothing to do;", vQKeyValue).c_str());
+	} break;
 	}
-
-	void tSimWindow::keyPressEvent(QKeyEvent *vQKeyEvent) {
-		auto vApp = static_cast<tApp *>(QApplication::instance());
-		vApp->fRunEventKeyPress(vQKeyEvent);
-		QWidget::keyPressEvent(vQKeyEvent);
-	}
-
-	//}
-
-	//{
-
-	//codetor
-
-	tSimReport::tSimReport(tSimWindow *vSimWindow)
-		: QWidget(vSimWindow)
-		, vLayout{new QVBoxLayout(this)}
-		, vStatus{new QTextEdit(this)}
-		, vScroll{new tSimReportScroll(this)} {
-		fLogErr("SimReport", "ctor");
-		this->vLayout->addWidget(this->vStatus.get(), 1);
-		this->vStatus->setReadOnly(1);
-
-		fAddGuiHorLine(this, this->vLayout);
-		this->vLayout->addWidget(this->vScroll.get(), 4);
-	}
-
-	//}
-
-	//{
-
-	//codetor
-
-	tSimReportScroll::tSimReportScroll(tSimReport *vSimReport)
-		: QScrollArea(vSimReport), vLayout{new QVBoxLayout(this)} {
-		fLogErr("SimReportScroll", "ctor");
-		this->setLayout(this->vLayout.get());
-	}
-
-	//}
 }
+
+//setters
+
+void tApp::fAddSimStep() {
+	this->vSimStepIndex++;
+}
+
+///}
+
+///{
+
+//codetor
+
+tAppWindow::tAppWindow(QWidget *vParentPtr, Qt::WindowFlags vFlags)
+	: QMainWindow(vParentPtr, vFlags)
+	, v1stWindow{new t1stWindow(this)}
+	, vSimWindow{new tSimWindow(this)}
+	, vStack{new QStackedWidget(this)} {
+	fLogErr("AppWindow", "ctor");
+	{//Stack
+
+		this->setCentralWidget(this->vStack.get());
+
+		this->vStack->addWidget(this->v1stWindow.get());
+		this->vStack->addWidget(this->vSimWindow.get());
+
+		this->vStack->setCurrentIndex(0);
+			connect(
+				static_cast<tApp *>(QApplication::instance()),
+				&tApp::sRunSimCall,
+				this,
+				&tAppWindow::sRunSimSlot
+			);
+	}
+	{//Fonts
+
+		auto vFontFpath = "data/FiraMono-Bold.ttf";
+		fLogErr("vFontFpath", "\"{}\";", vFontFpath);
+
+		auto vFontIndex = QFontDatabase::addApplicationFont(vFontFpath);
+		fLogErr("vFontIndex", "{};", vFontIndex);
+
+		auto vFontIdentArray = QFontDatabase::applicationFontFamilies(vFontIndex);
+		fLogErr("vFontIdentArray.length()", "{};", vFontIdentArray.length());
+
+		auto vFontIdent = vFontIdentArray.at(0);
+		fLogErr("vFontIdent", "\"{}\";", vFontIdent.toUtf8().constData());
+
+		auto vFontEntry = QFont(vFontIdent, 16);
+		vFontEntry.setBold(true);
+
+		this->setFont(vFontEntry);
+	}
+	{//Window show
+
+		this->setWindowTitle(dProjName);
+
+		auto vRect = QGuiApplication::primaryScreen()->geometry();
+		this->resize(vRect.width() / 2, vRect.height());
+
+		this->show();
+	}
+}
+
+//slots
+
+void tAppWindow::sRunSimSlot() {
+	this->vStack->setCurrentIndex(1);
+}
+
+///}
+
+///{
+
+//codetor
+
+t1stWindow::t1stWindow(tAppWindow *vAppWindow)
+	: QWidget(vAppWindow)
+	, vLayout{new QVBoxLayout(this)}
+	, vPrompt{new QTextEdit("start prompt", this)}
+	, vButton{new QPushButton("start", this)} {
+	fLogErr("1stWindow", "ctor");
+	this->setLayout(this->vLayout.get());
+
+	this->vLayout->addWidget(this->vPrompt.get(), 4);
+	this->vPrompt->setReadOnly(1);
+	this->vPrompt->setText(
+		"Welcome to the micro-organism simulation!\n"
+		"Before we run the simulation,"
+		" we want you to configure this."
+		"\n"
+		"The configuration is gonna start"
+		" as soon as you hit the button below."
+	);
+
+	this->vLayout->addWidget(this->vButton.get(), 1);
+	this->vButton->setText("Config");
+	connect(
+		this->vButton.get(), &QPushButton::clicked, this, &t1stWindow::fRunSimConfig
+	);
+}
+
+//slots
+
+void t1stWindow::fRunSimConfig() {
+	bool vInputFlag;
+	auto vSimBeing1stCount = QInputDialog::getInt(
+		this,
+		"Starting Simulation",
+		fmt::format(
+			"Enter the number of micro-organisms from {} to {}.",
+			this->vSimBeingMinCount,
+			this->vSimBeingMaxCount
+		)
+			.c_str(),
+		0,
+		this->vSimBeingMinCount,
+		this->vSimBeingMaxCount,
+		1,
+		&vInputFlag
+	);
+	if(vInputFlag) {
+		auto vApp = static_cast<tApp *>(QApplication::instance());
+		vApp->fRunSim(vSimBeing1stCount);
+	}
+}
+
+///}
+
+///{
+
+tSimWindow::tSimWindow(tAppWindow *vAppWindow)
+	: QWidget(vAppWindow)
+	, vLayout{new QVBoxLayout(this)}
+	, vPrompt{new QTextEdit("key prompt", this)}
+	, vOutput{new QTextEdit("key output", this)}
+	, vReport{new tSimReport(this)} {
+	fLogErr("SimWindow", "ctor");
+	this->setLayout(this->vLayout.get());
+
+	this->vLayout->addWidget(this->vPrompt.get(), 3);
+	this->vPrompt->setReadOnly(1);
+	this->vPrompt->setText(
+		"Hit one of the keys:"
+		"\n- 's' to add 1 step to the simulation progress;"
+		"\n- 'a' to use antibiotics;"
+		"\n- 'q' to quit;"
+	);
+
+	fAddGuiHorLine(this, this->vLayout);
+
+	this->vLayout->addWidget(this->vOutput.get(), 2);
+	this->vOutput->setReadOnly(1);
+
+	fAddGuiHorLine(this, this->vLayout);
+
+	this->vLayout->addWidget(this->vReport.get(), 8);
+}
+
+void tSimWindow::keyPressEvent(QKeyEvent *vQKeyEvent) {
+	auto vApp = static_cast<tApp *>(QApplication::instance());
+	vApp->fRunEventKeyPress(vQKeyEvent);
+	QWidget::keyPressEvent(vQKeyEvent);
+}
+
+///}
+
+///{
+
+//codetor
+
+tSimReport::tSimReport(tSimWindow *vSimWindow)
+	: QWidget(vSimWindow)
+	, vLayout{new QVBoxLayout(this)}
+	, vStatus{new QTextEdit(this)}
+	, vScroll{new tSimReportScroll(this)} {
+	fLogErr("SimReport", "ctor");
+	this->vLayout->addWidget(this->vStatus.get(), 1);
+	this->vStatus->setReadOnly(1);
+
+	fAddGuiHorLine(this, this->vLayout);
+	this->vLayout->addWidget(this->vScroll.get(), 4);
+}
+
+///}
+
+///{
+
+//codetor
+
+tSimReportScroll::tSimReportScroll(tSimReport *vSimReport)
+	: QScrollArea(vSimReport), vLayout{new QVBoxLayout(this)} {
+	fLogErr("SimReportScroll", "ctor");
+	this->setLayout(this->vLayout.get());
+}
+
+//slots
+
+void tSimReportScroll::sRunSimSlot() {
+}
+
+///}
+
+//}
 
 int main(int vArgC, char **vArgV) {
 	fmt::println(stderr, "[main]=(");
@@ -374,11 +389,13 @@ int main(int vArgC, char **vArgV) {
 		fLogErr("dProjName", "\"{}\";", dProjName);
 		fLogErr("dWorkPath", "\"{}\";", std::filesystem::current_path().c_str());
 		fLogErr("QT_VERSION_STR", "\"{}\";", QT_VERSION_STR);
-		auto vApp = tApp(vArgC, vArgV);
-		auto vErr = vApp.exec();
-		fLogErr("error", "{};", vErr);
-		if(vErr) {
-			return vErr;
+		{
+			auto vApp = tApp(vArgC, vArgV);
+			auto vErr = vApp.exec();
+			fLogErr("error", "{};", vErr);
+			if(vErr) {
+				return vErr;
+			}
 		}
 	} catch(const std::exception &vError) {
 		fLogErr("[exception]", "\"{}\";", vError.what());
--- src/exe.hpp
+++ src/exe.hpp
@@ -3,228 +3,244 @@
 #ifndef dChalGameOfLife_exe_hpp
 #define dChalGameOfLife_exe_hpp
 
-namespace {//simulation
+//{
 
-	/* simulation being class
-	 * - reproduces itself once in a constant amount of time
-	 * - dies after a constant amount of time
-	 * - can have a parent, inheriting it's timing settings
+/* simulation being class
+ * - reproduces itself once in a constant amount of time
+ * - dies after a constant amount of time
+ * - can have a parent, inheriting it's timing settings
+ */
+class tSimBeing {
+public://consdef
+
+	static constexpr auto vAntibioSurviveChance = 3;
+	static constexpr auto vAntibioKillingChance = 10;
+	static_assert(
+		vAntibioSurviveChance < vAntibioKillingChance,
+		"survival chance is included in the killing chance;"
+		"an organism survives if a random value [1,AntibioKillingChance]"
+		" is less than or equal to the AntibioSurviveChance;"
+		"an organism dies if a random value [1,AntibioKillingChance]"
+		" is more than the AntibioSurviveChance;"
+	);
+
+public://codetor
+
+	tSimBeing(std::optional<tSimBeing> vAncestor = {});
+
+public://actions
+
+	std::optional<tSimBeing> fRunStep();
+
+	bool fTryAntibio();
+
+private://actions
+
+	bool fTryDeath();
+
+	std::optional<tSimBeing> fTryRepro();
+
+public://getters
+
+	inline auto fGetAliveStepCount() const {
+		return this->vAliveStepCount;
+	}
+	inline auto fGetAliveStepLimit() const {
+		return this->vAliveStepLimit;
+	}
+
+	inline auto fGetReproStepCount() const {
+		return this->vReproStepCount;
+	}
+	inline auto fGetReproStepLimit() const {
+		return this->vReproStepLimit;
+	}
+	inline auto fGetReproIndex() const {
+		return this->vReproIndex;
+	}
+
+public://vetters
+
+	inline auto fVetAlive(bool vAliveValue = 1) const {
+		return (this->vAliveValue == vAliveValue);
+	}
+
+private://datadef
+
+	//lifetime
+	long			 vAliveStepCount;//how many steps since birth?
+	const long vAliveStepLimit;//how many steps until death?
+	bool			 vAliveValue;		 //are we alive?
+
+	//reproduction
+	long			 vReproStepCount;//how many steps since the last reproduction?
+	const long vReproStepLimit;//how many steps until the next reproduction?
+	const long vReproIndex;		 //the current generation number
+};
+
+class tSimThread: public QThread {
+public://codetor
+
+	tSimThread();
+
+public://actions
+
+	void run() override;
+};
+
+//}
+
+//{system
+
+/* application giga-class
+ * - creates and holds tAppWindow instance
+ */
+class tAppWindow;
+class tApp: public QApplication {
+	Q_OBJECT
+
+public://codetor
+
+	tApp(int vArgC, char **vArgV);
+
+public://actions
+
+	void fRunSim(int vSimBeing1stCount);
+
+	void fRunEventKeyPress(QKeyEvent *vQKeyEvent);
+
+signals:
+
+	void sRunSimCall();
+
+public://setters
+
+	void fAddSimStep();
+
+public://datadef
+
+	std::unique_ptr<tAppWindow> vWindow;
+
+	int vSimStepIndex;
+	int vSimBeing1stCount;
+
+	QVector<tSimBeing> vSimBeingArray;
+};
+
+/* application window:
+ * creates and holds tSimBeingReport;
+ */
+class t1stWindow;
+class tSimWindow;
+class tAppWindow: public QMainWindow {
+	Q_OBJECT
+
+public://codetor
+
+	tAppWindow(
+		QWidget *vParent = nullptr, Qt::WindowFlags vFlags = Qt::WindowFlags()
+	);
+
+public slots:
+
+	void sRunSimSlot();
+
+public://datadef
+
+	std::unique_ptr<t1stWindow> v1stWindow;
+	std::unique_ptr<tSimWindow> vSimWindow;
+
+	std::unique_ptr<QStackedWidget> vStack;
+};
+
+/* 1st window
+ * prompt users to input a number of simulation beings;
+ */
+class t1stWindow: public QWidget {
+	Q_OBJECT
+
+public://codetor
+
+	t1stWindow(tAppWindow *vAppWindow);
+
+public slots:
+
+	void fRunSimConfig();
+
+public://consdef
+
+	static constexpr auto vSimBeingMinCount = 1;
+	static constexpr auto vSimBeingMaxCount = 20;
+
+public://datadef
+
+	std::unique_ptr<QVBoxLayout> vLayout;
+
+	std::unique_ptr<QTextEdit>	 vPrompt;
+	std::unique_ptr<QPushButton> vButton;
+};
+
+/* simulation window
+ */
+class tSimReport;
+class tSimWindow: public QWidget {
+public://codetor
+
+	tSimWindow(tAppWindow *vAppWindow);
+
+public://actions
+
+	void keyPressEvent(QKeyEvent *vQKeyEvent) override;
+
+public://datadef
+
+	std::unique_ptr<QVBoxLayout> vLayout;
+
+	std::unique_ptr<QTextEdit> vPrompt;
+	std::unique_ptr<QTextEdit> vOutput;
+
+	std::unique_ptr<tSimReport> vReport;
+
+	/* window to monitor states of all organisms
 	 */
-	class tSimBeing {
-	public://consdef
+};
 
-		static constexpr auto vAntibioSurviveChance = 3;
-		static constexpr auto vAntibioKillingChance = 10;
-		static_assert(
-			vAntibioSurviveChance < vAntibioKillingChance,
-			"survival chance is included in the killing chance;"
-			"an organism survives if a random value [1,AntibioKillingChance]"
-			" is less than or equal to the AntibioSurviveChance;"
-			"an organism dies if a random value [1,AntibioKillingChance]"
-			" is more than the AntibioSurviveChance;"
-		);
+/* simulation report
+ * display information about the simulation
+ * hold scrolling area for simulation beings
+ */
+class tSimReportScroll;
+class tSimReport: public QWidget {
+public://codetor
 
-	public://codetor
+	tSimReport(tSimWindow *vSimWindow);
 
-		tSimBeing(std::optional<tSimBeing> vAncestor = {});
+public://datadef
 
-	public://getters
+	std::unique_ptr<QVBoxLayout>			vLayout;
+	std::unique_ptr<QTextEdit>				vStatus;
+	std::unique_ptr<tSimReportScroll> vScroll;
+};
 
-		inline auto fGetAliveStepCount() const {
-			return this->vAliveStepCount;
-		}
-		inline auto fGetAliveStepLimit() const {
-			return this->vAliveStepLimit;
-		}
+/* simulation report scroll
+ */
+class tSimReportScroll: public QScrollArea {
+	Q_OBJECT
 
-		inline auto fGetReproStepCount() const {
-			return this->vReproStepCount;
-		}
-		inline auto fGetReproStepLimit() const {
-			return this->vReproStepLimit;
-		}
-		inline auto fGetReproIndex() const {
-			return this->vReproIndex;
-		}
+public://codetor
 
-	public://vetters
+	tSimReportScroll(tSimReport *vSimReport);
 
-		inline auto fVetAlive(bool vAliveValue = 1) const {
-			return (this->vAliveValue == vAliveValue);
-		}
+public slots:
 
-	public slots:
+	void sRunSimSlot();
 
-		std::optional<tSimBeing> fRunStep();
+public://datadef
 
-		bool fTryAntibio();
+	std::unique_ptr<QVBoxLayout> vLayout;
+};
 
-	private://actions
-
-		bool fTryDeath();
-
-		std::optional<tSimBeing> fTryRepro();
-
-	private://datadef
-
-		//lifetime
-		long			 vAliveStepCount;//how many steps since birth?
-		const long vAliveStepLimit;//how many steps until death?
-		bool			 vAliveValue;		 //are we alive?
-
-		//reproduction
-		long			 vReproStepCount;//how many steps since the last reproduction?
-		const long vReproStepLimit;//how many steps until the next reproduction?
-		const long vReproIndex;		 //the current generation number
-	};
-
-	class tSimThread: public QThread {
-	public://codetor
-
-		tSimThread();
-
-	public://actions
-
-		void run() override;
-	};
-
-}
-
-namespace {//system
-
-	/* application giga-class
-	 * - creates and holds tAppWindow instance
-	 */
-	class tAppWindow;
-	class tApp: public QApplication {
-	public://codetor
-
-		tApp(int vArgC, char **vArgV);
-
-	public://actions
-
-    void fRunSim(int vSimBeing1stCount);
-
-		void fRunEventKeyPress(QKeyEvent *vQKeyEvent);
-
-	public://setters
-
-		void fAddSimStep();
-
-	public://datadef
-
-		std::unique_ptr<tAppWindow> vWindow;
-
-		int vSimStepIndex;
-    int vSimBeing1stCount;
-
-		QVector<tSimBeing> vSimBeingArray;
-	};
-
-	/* application window:
-	 * creates and holds tSimBeingReport;
-	 */
-	class t1stWindow;
-	class tSimWindow;
-	class tAppWindow: public QMainWindow {
-	public://codetor
-
-		tAppWindow(
-			QWidget *vParent = nullptr, Qt::WindowFlags vFlags = Qt::WindowFlags()
-		);
-
-	public slots:
-
-		void fSetSimWindow();
-
-	public://datadef
-
-		std::unique_ptr<t1stWindow> v1stWindow;
-		std::unique_ptr<tSimWindow> vSimWindow;
-
-		std::unique_ptr<QStackedWidget> vStack;
-	};
-
-	/* 1st window
-	 * prompt users to input a number of simulation beings;
-	 */
-	class t1stWindow: public QWidget {
-	public://codetor
-
-		t1stWindow(tAppWindow *vAppWindow);
-
-	public slots:
-
-		void fRunSimConfig();
-
-	public://consdef
-
-		static constexpr auto vSimBeingMinCount = 1;
-		static constexpr auto vSimBeingMaxCount = 20;
-
-	public://datadef
-
-		std::unique_ptr<QVBoxLayout> vLayout;
-
-		std::unique_ptr<QTextEdit>	 vPrompt;
-		std::unique_ptr<QPushButton> vButton;
-	};
-
-	/* simulation window
-	 */
-	class tSimReport;
-	class tSimWindow: public QWidget {
-	public://codetor
-
-		tSimWindow(tAppWindow *vAppWindow);
-
-	public://actions
-
-		void keyPressEvent(QKeyEvent *vQKeyEvent) override;
-
-	public://datadef
-
-		std::unique_ptr<QVBoxLayout> vLayout;
-
-		std::unique_ptr<QTextEdit> vPrompt;
-		std::unique_ptr<QTextEdit> vOutput;
-
-		std::unique_ptr<tSimReport> vReport;
-
-		/* window to monitor states of all organisms
-		 */
-	};
-
-	/* simulation report
-	 * display information about the simulation
-	 * hold scrolling area for simulation beings
-	 */
-	class tSimReportScroll;
-	class tSimReport: public QWidget {
-	public://codetor
-
-		tSimReport(tSimWindow *vSimWindow);
-
-	public://datadef
-
-		std::unique_ptr<QVBoxLayout>			vLayout;
-		std::unique_ptr<QTextEdit>				vStatus;
-		std::unique_ptr<tSimReportScroll> vScroll;
-	};
-
-	/* simulation report scroll
-	 */
-	class tSimReportScroll: public QScrollArea {
-	public://codetor
-
-		tSimReportScroll(tSimReport *vSimReport);
-
-	public://datadef
-
-		std::unique_ptr<QVBoxLayout> vLayout;
-	};
-
-}
+//}
 
 extern int main(int vArgC, char **vArgV);
 
